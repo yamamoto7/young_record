4fa35cc0397bc6e6ef492ca2744237f708d9dea8
KaiMinemura <minekai374@gmail.com>
2018-06-03 11:08:07 +0000

    create, update, redirect_to admins_genre_indexにしたよ

--- app/controllers/admins/genre_controller.rb
+++ app/controllers/admins/genre_controller.rb
@@ -3,15 +3,28 @@ class Admins::GenreController < ApplicationController
 	def create
 		genre = Genre.new(genre_param)
 		genre.save
-		# redirect_to genre一覧
+		redirect_to admins_genre_index
 	end
 
 	def index
 		@genre = Genre.new
 		@genres = Genre.all
 	end
+
 	def show
 		@genre = Genre.find(params[:id])
 		@items = @genre.items
 	end
+
+	def update
+		genre = Genre.find(params[:id])
+		genre.update(genre_params)
+		redirect_to admins_genre_index
+	end
+
+	private
+	def genre_params
+		params.require(:genre).permit(:name)
+	end
+
 end
