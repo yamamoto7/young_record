dd30c5b6042ae5a8f818a1b28f477a651ae856da
Kenta Yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-06-03 15:45:32 +0900

    create item new

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -3,9 +3,25 @@ class Admins::ItemsController < ApplicationController
 		
 	end
 	def new
+		@item = Item.new
+		@item.cds.build
 		
 	end
 	def edit
+		
+	end
+	def create
+		item = Item.new(item_params)
+		item.save
+		redirect_to root_path
+	end
+
+	private
+	def item_params
+		params.require(:item).permit(:price, :count, :cd_number, :name, :artist_name, :label_name, :text_id, :frag,
+			cds_attributes: [:id, :name],
+			songs_attributes: [:id, :name]
+			)
 		
 	end
 end
