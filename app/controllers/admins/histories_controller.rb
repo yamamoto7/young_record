5c3b9f0d41d67c79958e46f9254dc3ff73a22869
KaiMinemura <minekai374@gmail.com>
2018-06-08 20:15:06 +0000

    models/history.rb #scope

--- app/controllers/admins/histories_controller.rb
+++ app/controllers/admins/histories_controller.rb
@@ -1,6 +1,9 @@
 class Admins::HistoriesController < ApplicationController
 	def index
 		@histories = History.all
+		if params[:status].present?
+			@histories = @histories.get_by_status params[:status]
+		end
 	end
 	def show
 		@history = History.find(oarams[:id])
