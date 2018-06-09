4c83a772282affe81b85a50325e1074e5de51719
KaiMinemura <minekai374@gmail.com>
2018-06-09 03:41:12 +0000

    complete views/histories/index.html.erb

--- app/controllers/admins/histories_controller.rb
+++ app/controllers/admins/histories_controller.rb
@@ -1,11 +1,18 @@
 class Admins::HistoriesController < ApplicationController
+
 	def index
 		@histories = History.all
 		if params[:status].present?
 			@histories = @histories.get_by_status params[:status]
 		end
 	end
+
 	def show
 		@history = History.find(oarams[:id])
 	end
+
+	def update
+		@history = Hi
+	end
+
 end
