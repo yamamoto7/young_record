7e944d367eb6fbab6cca6f471a05309f2ca53412
tatsuki1116 <tatsuki1617@gmail.com>
2018-08-11 12:23:54 +0000

    ごめんなさい

--- app/controllers/[scope]/registrations_controller.rb
+++ app/controllers/[scope]/registrations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
+class [scope]::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -59,8 +59,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-
-    def after_update_path_for(resource)
-      items_path
-    end
 end
